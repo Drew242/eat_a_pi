26e66b37e1501cab724da900451d77e39cdcacc9
Drew Conly <atconly@gmail.com>
2016-04-18 21:31:37 -0600

    fix saving bug and update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   def self.find_or_create_from_auth(oauth_data)
     user = User.find_or_create_by(provider: oauth_data.provider, uid: oauth_data.uid)
 
-    user.username        = oauth_data.username
-    user.access_token    = oauth_data.oauth_token
-    user.access_secret   = oauth_data.oauth_verifier
-    return user
+    user.username        = oauth_data.info.nickname
+    user.access_token    = oauth_data.credentials.token
+    user.access_secret   = oauth_data.credentials.secret
+    user.save
+
+    user
   end
 end
