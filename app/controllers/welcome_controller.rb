04248d468a9d0bc592712174432520f868548f03
Drew Conly <atconly@gmail.com>
2016-04-20 17:21:49 -0600

    begin creating array of picture urls

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def landing
+    @pies = pie_urls
   end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,17 @@
+require 'flickraw'
+
 module WelcomeHelper
+  FlickRaw.api_key       = ENV['FLICKR_ID']
+  FlickRaw.shared_secret = ENV['FLICKR_SECRET']
+
+  args               = {}
+  args[:safe_search] = 2
+  args[:text]        = "pies OR pi"
+  pie_search         = flickr.photos.search(args)
+  pie_ids            = pie_search.map { |p| p.id }
+  pie_info           = pie_ids.map { |p| flickr.photos.getInfo(:photo_id => "#{p}")}
+  pie_urls           = pie_info.map {|p| FlickRaw.url_s(p)}
+
+  # info = flickr.photos.getInfo(:photo_id => "3839885270")
+  # FlickRaw.url_b(info) # => "https://farm3.static.flickr.com/2485/3839885270_6fb8b54e06_b.jpg"
 end
